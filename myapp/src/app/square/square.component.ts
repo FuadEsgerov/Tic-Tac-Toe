7b53b0d163425578e46a402a8263751b78d27fab
FuadEsgerov <fuadra@code.edu.az>
2020-09-11 18:15:44 +0400

    Created App

---
+++ myapp/src/app/square/square.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.scss']
+})
+export class SquareComponent  {
+
+  @Input() value: 'X' | 'O';
+
+}
